525f9920c188e8f89a48554209eab671d948ecfb
PEDRO_5245 <PEDRO_5245@uirapuru.br>
2026-03-03 19:13:40 -0300

    app

---
+++ app/app/src/main/java/android/myapplication/MainActivity.java
@@ -0,0 +1,46 @@
+package android.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+EditText EdtText;
+Button login;
+
+Button btnGoogle;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        initComponents();
+
+        //Evento para trocar de página da main para a tela de login
+        login.setOnClickListener(V ->{
+            Intent intent = new Intent(MainActivity.this, tela_login.class);
+            startActivity(intent);
+        });
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    private void initComponents() {
+        EdtText   = findViewById(R.id.edt_email);
+        btnGoogle = findViewById(R.id.btn_google);
+        login     = findViewById(R.id.btn_Login);
+    }
+}
